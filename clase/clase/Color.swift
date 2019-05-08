4525624e29190eba0e1d9bfa918ddaf6b31b7035
Javiergzhdz <javiergzhdz@gmail.com>
2019-05-08 12:42:46 -0500

    Add files via upload
    Clase 8 de mayo del 2019

---
+++ clase/clase/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  clase
+//
+//  Created by Macbook on 5/8/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import Foundation
+struct Color{//En clase se cambia todo, en struct se hace una copia
+    var Nombre: String
+    var Composicion: [String]?//como en los colores primarios no va haber esto pues se pone asi 
+    
+}
---
+++ clase/clase/ColorTableViewController.swift
@@ -0,0 +1,118 @@
+//
+//  ColorTableViewController.swift
+//  clase
+//
+//  Created by Macbook on 5/8/19.
+//  Copyright © 2019 ioslab. All rights reserved.
+//
+
+import UIKit
+
+class ColorTableViewController: UITableViewController {
+    //Hacer un arreglo de tipo color
+    let coloresPrimarios: [Color] = [
+        Color(Nombre: "Azul", Composicion: nil),
+        Color(Nombre: "Rojo", Composicion: nil),
+        Color(Nombre: "Amarillo", Composicion: nil)
+    ]
+    let coloresSecundarios: [Color] = [
+        Color(Nombre: "Morado", Composicion: ["Rojo","Azul"]),
+        Color(Nombre: "Naranja", Composicion: ["Rojo","Amarillo"]),
+        Color(Nombre: "Verde", Composicion: ["Azul","Amarillo"])
+    ]
+    let coloresTerciarios: [Color] = [
+        Color(Nombre: "Azul Turquesa", Composicion: ["Verde","Azul"]),
+                Color(Nombre: "Amarillo Huevo", Composicion: ["Amarillo","Naranja"])
+       
+    ]
+    let secciones = ["Primarios","Secundarios","Terciarios"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return secciones.count//regresa el numero de secciones
+        //si no lo ponemos regresa 1
+    }
+   
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        if section == 0{
+            return coloresPrimarios.count
+        }else if section == 1{
+            return coloresSecundarios.count
+        } else if section == 2 {
+            return coloresTerciarios.count
+        } else {
+            return 0
+        }
+        //regresa el nombre que esta en la posicion section
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
